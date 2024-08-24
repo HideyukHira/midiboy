2d2a499f9ab7d9efe6428707327451948dbeae31
HideyukiHira <hidexallpowers@gmail.com>
2024-08-24 18:44:21 +0900

    MIDIデバイスからの入力をキーボード入力に変換する機能を追加
    - Note On イベントをキーの押下に変換
    - Note Off イベントをキーのリリースに変換
    - ノート番号をキーコードにマッピングするロジックを実装

--- midiboy/Form1.cs
+++ midiboy/Form1.cs
@@ -3,8 +3,6 @@ using System.Diagnostics;
 using System.Windows.Forms;
 using NAudio.Midi;
 
-
-
 namespace midiboy
 {
     public partial class Form1 : Form
@@ -12,16 +10,13 @@ namespace midiboy
         private MidiIn midiIn;
         private ComboBox comboBoxMidi;
 
-
         public Form1()
         {
-            // comboBoxMidi へ comboBox1 を代入
-
             InitializeComponent();
+            // comboBoxMidi へ comboBox1 を代入
             comboBoxMidi = comboBox1;
+            comboBoxMidi.SelectedIndexChanged += ComboBoxMidi_SelectedIndexChanged;
             LoadMidiDevices();
-
-
 
             // デバッグ出力にMIDIデバイスのリストを出力
             Debug.WriteLine("Available MIDI Input Devices:");
@@ -29,13 +24,8 @@ namespace midiboy
             {
                 Debug.WriteLine($"{device}: {MidiIn.DeviceInfo(device).ProductName}");
             }
-   
-
-  
-
         }
 
- 
         private void LoadMidiDevices()
         {
             // MIDIデバイスを読み込んでコンボボックスに追加
@@ -46,41 +36,79 @@ namespace midiboy
             {
                 string deviceName = MidiIn.DeviceInfo(i).ProductName;
                 comboBoxMidi.Items.Add(deviceName);
-
-
             }
 
             if (deviceCount == 0)
             {
                 comboBoxMidi.Items.Add("No MIDI devices found");
             }
+        }
 
-            // MIDIデバイスの選択
-  
-            
-                midiIn = new MidiIn(1);
+        private void ComboBoxMidi_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // 選択されたMIDIデバイスのインデックスを取得
+            int selectedIndex = comboBoxMidi.SelectedIndex;
+            if (selectedIndex >= 0 && selectedIndex < MidiIn.NumberOfDevices)
+            {
+                // 既存のMIDIデバイスを停止して破棄
+                if (midiIn != null)
+                {
+                    midiIn.Stop();
+                    midiIn.Dispose();
+                }
+
+                // 新しいMIDIデバイスを選択
+                midiIn = new MidiIn(selectedIndex);
                 midiIn.MessageReceived += MidiIn_MessageReceived;
                 midiIn.ErrorReceived += MidiIn_ErrorReceived;
                 midiIn.Start();
-            
-        
-
-
-
+            }
         }
-
 
         private void MidiIn_MessageReceived(object sender, MidiInMessageEventArgs e)
         {
-            // MIDIメッセージをTextBoxに表示
-            Invoke(new Action(() =>
+            if (e.MidiEvent is NoteOnEvent noteOnEvent)
             {
-                // MIDIイベントを適切な形式で表示
-                textBox1.AppendText($"MIDI Message Received: {e.MidiEvent}\r\n");
-                // MIDIメッセージのデータを コンソールに表示
-                Debug.WriteLine($"MIDI Message Received: {e.MidiEvent}\r\n");
-            }));
+                // Note On イベントを処理
+                HandleNoteOn(noteOnEvent);
+            }
+            else if (e.MidiEvent is NoteEvent noteEvent && noteEvent.CommandCode == MidiCommandCode.NoteOff)
+            {
+                // Note Off イベントを処理
+                HandleNoteOff(noteEvent);
+            }
         }
+
+        private void HandleNoteOn(NoteOnEvent noteOnEvent)
+        {
+            // ノート番号をキーコードに変換
+            Keys key = ConvertNoteToKey(noteOnEvent.NoteNumber);
+            // キーの押下をシミュレート
+            SendKeys.SendWait(key.ToString());
+        }
+
+        private void HandleNoteOff(NoteEvent noteEvent)
+        {
+            // ノート番号をキーコードに変換
+            Keys key = ConvertNoteToKey(noteEvent.NoteNumber);
+            // キーのリリースをシミュレート（必要に応じて実装）
+            // SendKeys.SendWait("{UP " + key.ToString() + "}");
+        }
+
+        private Keys ConvertNoteToKey(int noteNumber)
+        {
+            // ノート番号をキーコードに変換するロジックを実装
+            // 例: C4 (ノート番号60) を A キーにマッピング
+            switch (noteNumber)
+            {
+                case 60: return Keys.A;
+                case 61: return Keys.W;
+                case 62: return Keys.S;
+                // 他のノート番号も同様にマッピング
+                default: return Keys.None;
+            }
+        }
+
 
         private void MidiIn_ErrorReceived(object sender, MidiInMessageEventArgs e)
         {
